cf1e377671f949a0b4349caa56251e014c57a9c9
andjela24 <1andjela.ljubenkovic@gmail.com>
2024-07-03 15:38:07 +0200

    fixed routes

--- src/customer/components/carousel/HomeCarouselData.js
+++ src/customer/components/carousel/HomeCarouselData.js
@@ -1,19 +1,19 @@
 export const homeCarouselData=[
     {
         image: "https://res.cloudinary.com/do7xoj7ks/image/upload/v1719479573/Banner3-1-min_owl1vd.png",
-        path: "/woman/clothing/lengha_choli"
+        path: "cakes?category=svadbene"
     },
     {
         image: "https://res.cloudinary.com/do7xoj7ks/image/upload/v1719479573/Banner4-1-min_cw7mvc.png",
-        path: "/woman/clothing/women_dress"
+        path: "/cakes?category=dečije"
     },
     {
         image: "https://res.cloudinary.com/do7xoj7ks/image/upload/v1719479573/Baner2-1-min_t3ibhb.png",
-        path: "/woman/clothing/women_dress"
+        path: "/cakes?category=specijal"
     },
     {
         image: "https://res.cloudinary.com/do7xoj7ks/image/upload/v1719479573/Banner1-1-min_uura1h.png",
-        path: "/woman/clothing/women_saree"
+        path: "/cakes?category=svečane"
     },
 
 ]
